b0a657661d87f815392288120bad677125b761da
Brian Stack <bis12@case.edu>
2013-03-21 17:54:17 -0400

    Make it more d3-y

--- dopplerganger/static/js/doppler.js
+++ dopplerganger/static/js/doppler.js
@@ -9,14 +9,27 @@ function run() {
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(.55)")
       .append("g");
 
+    svg.append("g").attr("class", "darts");
+
     var connection = new WebSocket('ws://' + window.location.host + '/dots');
 
     connection.onmessage = function(evt) {
-        var point = JSON.parse(evt.data);
-        svg.append("circle")
-                .attr("cx", point.x)
-                .attr("cy", point.y)
-                .attr("r", 2.5);
-    }
+        // TODO: This is all bad
+        var data = [];
+        data.push(JSON.parse(evt.data));
+        console.log(data);
+        var darts = svg.select(".darts").selectAll("circle").data(data);
+        console.log(darts);
 
+        darts.enter().insert("circle")
+            .attr("cx", function(p) {return p.x})
+            .attr("cy", function(p) {return p.y})
+            .attr("r", 2.5)
+            .attr("fill-opacity", 1.0)
+            .transition()
+            .duration(400)
+            .attr("r", 6)
+            .attr("fill-opacity", 0.0)
+            .remove();
+    }
 }
